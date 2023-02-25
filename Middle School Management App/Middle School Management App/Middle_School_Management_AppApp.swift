dad6b323f63585b278c74de465c32ec3013343cf
= <yee.vivianne@gmail.com>
2023-02-25 10:08:42 -0500

    create new xcode project

---
+++ Middle School Management App/Middle School Management App/Middle_School_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Middle_School_Management_AppApp.swift
+//  Middle School Management App
+//
+//  Created by Vivianne Yee on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct Middle_School_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
