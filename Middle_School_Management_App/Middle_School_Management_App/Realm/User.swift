5c09452390a9b4aa8f1dc22ee011e122d847b63f
= <yee.vivianne@gmail.com>
2023-03-27 17:39:31 -0400

    basic schema

---
+++ Middle_School_Management_App/Middle_School_Management_App/Realm/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  Middle_School_Management_App
+//
+//  Created by Vivianne Yee on 2023-03-27.
+//
+
+import Foundation
+import RealmSwift
+
+class User: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var email: String
+    @Persisted var password: String
+}
+
+class Teacher: User {
+    @Persisted var name: String
+    @Persisted var classes: List<Class>
+}
+
+class Student: User {
+    @Persisted var classes: List<Class>
+}
