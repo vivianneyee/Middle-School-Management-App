cad6c56474f89f376fe55068dc97aaf0f1014653
= <yee.vivianne@gmail.com>
2023-04-07 21:53:47 -0400

    fix post and class objects and add crud functions

--- Middle_School_Management_App/Middle_School_Management_App/Class.swift
+++ Middle_School_Management_App/Middle_School_Management_App/Class.swift
@@ -9,16 +9,96 @@ import Foundation
 import RealmSwift
 
 class Class: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var name: String = ""
     @Persisted var color: String = ""
     @Persisted var code: String = ""
-    @Persisted var posts: List<Post>
+    @Persisted var events: List<Event>
+    @Persisted var assignments: List<Assignment>
+    @Persisted var alerts: List<Alert>
+    @Persisted var studentInputs: List<StudentInput>
     
     convenience init(name: String, color: String, code: String) {
         self.init()
         self.name = name
         self.color = color
         self.code = code
+    }
+}
+
+class ClassDataAccess {
+    private let realm: Realm
+    
+    init(realm: Realm) {
+        self.realm = realm
+    }
+    
+    // create a new class
+    func createClass(name: String, color: String) {
+        let code = generateClassCode(realm: realm)
+        let newClass = Class(name: name, color: color, code: code)
+        try! realm.write {
+             realm.add(newClass)
+        }
+    }
+    
+    // delete a class
+    func deleteClass(classObj: Class) {
+        try! realm.write {
+            realm.delete(classObj)
+        }
+    }
+    
+    // update a class
+    func updateClass(classObj: Class, name: String, color: String) {
+        try! realm.write {
+            classObj.name = name
+            classObj.color = color
+        }
+    }
+    
+    // return all classes in db
+    func getAllClasses() -> Results<Class> {
+        return realm.objects(Class.self)
+    }
+    
+    // get a specific class by its code
+    func getClassByCode(code: String) -> Class {
+        let classes = getAllClasses()
+        let classObj = classes.where {
+            $0.code == code
+        }[0]
+        return classObj
+    }
+    
+    // ***** FOR TESTING *****
+    func getFirstClass() -> Class {
+        let classes = getAllClasses()
+        return classes[0]
+    }
+    
+    // generate random 6-character code made up of 3 letters and 3 numbers
+    private func generateCode() -> String {
+        var code = ""
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let nums = "1234567890"
+        for _ in 0..<3 {
+            let letterIndex = Int.random(in: 0..<letters.count)
+            code.append(letters[letters.index(letters.startIndex, offsetBy: letterIndex)])
+        }
+        for _ in 0..<3 {
+            let numIndex = Int.random(in: 0..<nums.count)
+            code.append(nums[nums.index(nums.startIndex, offsetBy: numIndex)])
+        }
+        
+        return code
+    }
+    
+    // use generateCode() to create a random class code and ensure it is not already in use
+    private func generateClassCode(realm: Realm) -> String {
+        var code = generateCode()
+        while realm.objects(Class.self).filter("code == %@", code).count > 0 {
+            code = generateCode()
+        }
+        return code
     }
 }
--- Middle_School_Management_App/Middle_School_Management_App/Post.swift
+++ Middle_School_Management_App/Middle_School_Management_App/Post.swift
@@ -13,62 +13,107 @@ class Post: Object {
     @Persisted var title: String
     @Persisted var date: Date
     @Persisted var postType: String
-    
-    convenience init(title: String, date: Date, postType: String) {
-        self.init()
-        self.title = title
-        self.date = date
-        self.postType = postType
-    }
 }
 
 class Event: Post {
     @Persisted var files: List<String>
-    
-    convenience init(title: String, date: Date, files: List<String>) {
-        self.init()
-        self.title = title
-        self.date = date
-        self.postType = "EVENT"
-        self.files = files
-    }
 }
 
 class Assignment: Post {
     @Persisted var files: List<String>
-    
-    convenience init(title: String, date: Date, files: List<String>) {
-        self.init()
-        self.title = title
-        self.date = date
-        self.postType = "ASSIGNMENT"
-        self.files = files
-    }
 }
 
 class Alert: Post {
-    @Persisted var files: List<String>
-    
-    convenience init(title: String, date: Date, files: List<String>) {
-        self.init()
-        self.title = title
-        self.date = date
-        self.postType = "ALERT"
-        self.files = files
-    }
+    @Persisted var priority: String
 }
 
 class StudentInput: Post {
     @Persisted var inputType: String
     @Persisted var responses: List<String>
+}
+
+class PostDataAccess {
     
-    convenience init(title: String, date: Date, inputType: String) {
-        self.init()
-        self.title = title
-        self.date = date
-        self.postType = "STUDENTINPUT"
-        self.inputType = inputType
-        self.responses = List<String>()
+    private let realm: Realm
+    
+    init(realm: Realm) {
+        self.realm = realm
+    }
+    
+    // create a new Event object
+    func createEvent(classObj: Class, title: String, date: Date, files: List<String>) {
+        let newEvent = Event()
+        newEvent.title = title
+        newEvent.date = date
+        newEvent.files = files
+        newEvent.postType = "EVENT"
+        
+        try! realm.write {
+            realm.add(newEvent)
+            classObj.events.append(newEvent)
+        }
+    }
+    
+    // create a new Assignment object
+    func createAssignment(classObj: Class, title: String, date: Date, files: List<String>) {
+        let newAssignment = Assignment()
+        newAssignment.title = title
+        newAssignment.date = date
+        newAssignment.files = files
+        newAssignment.postType = "ASSIGNMENT"
+        
+        try! realm.write {
+            realm.add(newAssignment)
+            classObj.assignments.append(newAssignment)
+        }
+    }
+    
+    // create new Alert object
+    func createAlert(classObj:Class, title: String, date: Date, priority: String) {
+        let newAlert = Alert()
+        newAlert.title = title
+        newAlert.date = date
+        newAlert.priority = priority
+        newAlert.postType = "ALERT"
+        
+        try! realm.write {
+            realm.add(newAlert)
+            classObj.alerts.append(newAlert)
+        }
+    }
+    
+    // create a new StudnetInput object
+    func createStudentInput(classObj:Class, title: String, date: Date, inputType: String) {
+        let newStudentInput = StudentInput()
+        newStudentInput.title = title
+        newStudentInput.date = date
+        newStudentInput.inputType = inputType
+        newStudentInput.postType = "STUDENTINPUT"
+        
+        try! realm.write {
+            realm.add(newStudentInput)
+            classObj.studentInputs.append(newStudentInput)
+        }
     }
 
+    // read all events belonging to a class
+    func getEvents(classObj: Class) -> [Event] {
+        return Array(classObj.events)
+    }
+    
+    // read all assignments belonging to a class
+    func getAssignments(classObj: Class) -> [Assignment] {
+        return Array(classObj.assignments)
+    }
+    
+    // read all alerts belonging to a class
+    func getAlerts(classObj: Class) -> [Alert] {
+
+        return Array(classObj.alerts)
+    }
+
+    // read all student inputs belonging to a class
+    func getStudentInputs(classObj: Class) -> [StudentInput] {
+        return Array(classObj.studentInputs)
+    }
 }
