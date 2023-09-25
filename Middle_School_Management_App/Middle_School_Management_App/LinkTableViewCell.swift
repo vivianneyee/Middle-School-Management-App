c6cf76316d6aa54f6d192a7d3e21a49fdfb1c000
Vivienne Cruz <52329335+viviennecruz@users.noreply.github.com>
2023-09-25 16:02:02 -0400

    added missing custom ui classes

---
+++ Middle_School_Management_App/Middle_School_Management_App/LinkTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FourLinksTableViewCell.swift
+//  Middle_School_Management_App
+//
+//  Created by Vivienne Cruz on 2023-04-04.
+//
+
+import UIKit
+
+class LinkTableViewCell: UITableViewCell {
+    @IBOutlet weak var labelName: UILabel!
+    
+}
