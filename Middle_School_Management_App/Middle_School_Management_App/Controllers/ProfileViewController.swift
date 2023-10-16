47a29f78a1800a0dcb0c5b584c5ae3355e0dbf8e
Vivienne Cruz <cruzviv27@hotmail.com>
2023-10-15 20:14:06 -0400

    login signup ui and reorganized vc

---
+++ Middle_School_Management_App/Middle_School_Management_App/Controllers/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  Middle_School_Management_App
+//
+//  Created by Vivienne Cruz on 2023-10-15.
+//
+import UIKit
+import ECWeekView
+import SwiftDate
+import RealmSwift
+
+class ProfileViewController: UIViewController {
+    
+    @IBAction func tapToSettings(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "settings") as! SettingsViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func tapToNotifPref(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "notifpref") as! NotificationPrefViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func tapToLogin(_ sender: Any) {
+       let vc = storyboard?.instantiateViewController(identifier: "login") as! LoginViewController
+//       navigationController?.pushViewController(vc, animated: true)
+       // add sign out logic
+       vc.modalPresentationStyle = .fullScreen  // Optional: Set the presentation style
+       present(vc, animated: true, completion: nil)
+   }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ Middle_School_Management_App/Middle_School_Management_App/Controllers/SettingsViewController.swift
@@ -0,0 +1,182 @@
+//
+//  SettingsViewController.swift
+//  Middle_School_Management_App
+//
+//  Created by Vivienne Cruz on 2023-10-15.
+//
+
+import Foundation
+import UIKit
+import ECWeekView
+import SwiftDate
+import RealmSwift
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+        
+        do {
+            let realm = try Realm()
+            
+            // for debugging
+            print(Realm.Configuration.defaultConfiguration.fileURL!)
+        } catch let error as NSError {
+            print("Error initializing Realm: \(error.localizedDescription)")
+        }
+    }
+
+    func createClass(realm: Realm, name: String, color: String) {
+        let code = generateClassCode(realm: realm)
+        let newClass = Class(name: name, color: color, code: code)
+        try! realm.write {
+             realm.add(newClass)
+        }
+    }
+    
+    // generate random 6-character code made up of 3 letters and 3 numbers
+    func generateCode() -> String {
+        var code = ""
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let nums = "1234567890"
+        for _ in 0..<3 {
+            let letterIndex = Int.random(in: 0..<letters.count)
+            code.append(letters[letters.index(letters.startIndex, offsetBy: letterIndex)])
+        }
+        for _ in 0..<3 {
+            let numIndex = Int.random(in: 0..<nums.count)
+            code.append(nums[nums.index(nums.startIndex, offsetBy: numIndex)])
+        }
+        
+        return code
+    }
+    
+    // use generateCode() to create a random class code and ensure it is not already in use
+    func generateClassCode(realm: Realm) -> String {
+        var code = generateCode()
+        while realm.objects(Class.self).filter("code == %@", code).count > 0 {
+            code = generateCode()
+        }
+        return code
+    }
+    
+    func weekViewDidClickOnEvent(_ weekView: ECWeekView, event: ECWeekViewEvent, view: UIView) {
+        //        eventDetailLauncher.event = event
+        //        eventDetailLauncher.present()
+    }
+    
+    func weekViewDidClickOnFreeTime(_ weekView: ECWeekView, date: DateInRegion) {
+        print(#function, "date:", date.toString())
+    }
+    
+    // Creates the view for an event
+//    func weekViewStylerECEventView(_ weekView: ECWeekView, eventContainer: CGRect, event: ECWeekViewEvent) -> UIView {
+//        let eventView = UIView(frame: eventContainer)
+//        eventView.layer.cornerRadius = 10
+//        eventView.layer.masksToBounds = true
+//
+//        // Create a dictionary of classes and their colors
+//        let classes = [
+//            "Math": UIColor.red,
+//            "6B - Art": UIColor.blue,
+//            "6A - Art": UIColor.blue,
+//            "Gym": UIColor.green,
+//            "Music": UIColor.yellow,
+//            "French": UIColor.purple,
+//            "Social Studies": UIColor.orange,
+//            "Planning Time": UIColor.cyan,
+//            "Languages": UIColor.systemPink
+//        ]
+//
+//
+//        if let classColor = classes[event.title] {
+//            print(classes)
+//            eventView.backgroundColor = UIColor.clear
+//            eventView.layer.borderColor = classColor.cgColor // Set border color to match background color
+//            eventView.layer.borderWidth = 1.0 // Set border width to 1.0 point
+//        } else {
+//            eventView.backgroundColor = UIColor.gray
+//        }
+//    }
+    
+    var font: UIFont = UIFont.systemFont(ofSize: 10)
+    
+    var showsDateHeader: Bool = true
+    
+    var dateHeaderHeight: CGFloat = 12
+    
+    
+    
+    // Creates the view for an event
+    func weekViewStylerECEventView(_ weekView: ECWeekView, eventContainer: CGRect, event: ECWeekViewEvent) -> UIView {
+        let eventView = UIView(frame: eventContainer)
+        eventView.layer.cornerRadius = 0
+        eventView.layer.masksToBounds = true
+        
+        // Create a dictionary of classes and their colors
+        let classes = [
+            "Math": UIColor.red,
+            "6B - Art": UIColor.blue,
+            "6A - Art": UIColor.blue,
+            "Gym": UIColor.green,
+            "Music": UIColor.yellow,
+            "French": UIColor.purple,
+            "Social Studies": UIColor.orange,
+            "Planning Time": UIColor.cyan,
+            "Languages": UIColor.systemPink
+        ]
+        
+        
+        if let classColor = classes[event.title] {
+            print(classes)
+            eventView.backgroundColor = classColor.withAlphaComponent(0.25)
+            eventView.layer.borderColor = classColor.withAlphaComponent(0.25).cgColor // Set border color to match background color
+            eventView.layer.borderWidth = 1.0 // Set border width to 1.0 point
+            
+            if eventView.backgroundColor == UIColor.blue || eventView.backgroundColor == UIColor.purple {
+                let labelTitle = UILabel(frame: CGRect(x: 5, y: 2, width: eventView.bounds.width - 5, height: eventView.bounds.height / 2))
+                let labelSub = UILabel(frame: CGRect(x: 5, y: eventView.bounds.height / 2, width: eventView.bounds.width - 5, height: eventView.bounds.height / 2))
+                labelTitle.text = event.title
+                labelTitle.font = font
+                labelTitle.textColor = UIColor.white // Change text color as desired
+                labelTitle.textAlignment = .left
+                labelTitle.numberOfLines = 0
+                labelTitle.sizeToFit()
+                
+                labelSub.text = event.subtitle
+                labelSub.font = font
+                labelSub.textColor = UIColor.white // Change text color as desired
+                labelSub.textAlignment = .left
+                labelSub.numberOfLines = 0
+                labelSub.sizeToFit()
+                
+                eventView.addSubview(labelTitle)
+                eventView.addSubview(labelSub)
+            } else {
+                let labelTitle = UILabel(frame: CGRect(x: 5, y: 2, width: eventView.bounds.width - 5, height: eventView.bounds.height / 2))
+                let labelSub = UILabel(frame: CGRect(x: 5, y: eventView.bounds.height / 2, width: eventView.bounds.width - 5, height: eventView.bounds.height / 2))
+                labelTitle.text = event.title
+                labelTitle.font = font
+                labelTitle.textColor = UIColor.black // Change text color as desired
+                labelTitle.textAlignment = .left
+                labelTitle.numberOfLines = 0
+                labelTitle.sizeToFit()
+                
+                labelSub.text = event.subtitle
+                labelSub.font = font
+                labelSub.textColor = UIColor.black // Change text color as desired
+                labelSub.textAlignment = .left
+                labelSub.numberOfLines = 0
+                labelSub.sizeToFit()
+                
+                eventView.addSubview(labelTitle)
+                eventView.addSubview(labelSub)
+            }
+        } else {
+            eventView.backgroundColor = UIColor.gray
+        }
+        
+        return eventView
+    }
+}
