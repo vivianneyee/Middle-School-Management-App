3bb7db28aa7820a32e3757199149e531edebf9a4
Vivienne Cruz <cruzviv27@hotmail.com>
2023-11-20 15:01:24 -0500

    added validation for signup

--- Middle_School_Management_App/Middle_School_Management_App/Controllers/SignupViewController.swift
+++ Middle_School_Management_App/Middle_School_Management_App/Controllers/SignupViewController.swift
@@ -11,13 +11,21 @@ import ECWeekView
 import SwiftDate
 import RealmSwift
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
     // Email or username input field
     private let emailTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Email or Username"
         textField.borderStyle = .roundedRect
         return textField
+    }()
+    
+    // email error label
+    private let emailErrorLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
     }()
 
     // Password input field
@@ -27,6 +35,14 @@ class SignupViewController: UIViewController {
         textField.borderStyle = .roundedRect
         textField.isSecureTextEntry = true
         return textField
+    }()
+    
+    // password error label
+    private let passwordErrorLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
     }()
 
     // Login button
@@ -47,6 +63,14 @@ class SignupViewController: UIViewController {
         textField.isSecureTextEntry = true
         return textField
     }()
+    
+    // confirm password error label
+    private let confirmPasswordErrorLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,35 +78,58 @@ class SignupViewController: UIViewController {
         
         // Add the action for the loginButton
         signupButton.addTarget(self, action: #selector(signupButtonTapped), for: .touchUpInside)
+        emailTextField.delegate = self
+
 
        // Add subviews
        view.addSubview(emailTextField)
+       view.addSubview(emailErrorLabel)
+       view.addSubview(passwordErrorLabel)
+       view.addSubview(confirmPasswordErrorLabel)
        view.addSubview(passwordTextField)
        view.addSubview(confirmPasswordTextField)
        view.addSubview(signupButton)
 
        // Set up constraints
        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+       emailErrorLabel.translatesAutoresizingMaskIntoConstraints = false
        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+       passwordErrorLabel.translatesAutoresizingMaskIntoConstraints = false
        signupButton.translatesAutoresizingMaskIntoConstraints = false
        confirmPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+       confirmPasswordErrorLabel.translatesAutoresizingMaskIntoConstraints = false
 
        NSLayoutConstraint.activate([
            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
            emailTextField.widthAnchor.constraint(equalToConstant: 200),
            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+           
+           emailErrorLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 2),
+           emailErrorLabel.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+           emailErrorLabel.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+           emailErrorLabel.heightAnchor.constraint(equalToConstant: 20),
 
            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
            passwordTextField.widthAnchor.constraint(equalToConstant: 200),
            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
            
+           passwordErrorLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 2),
+           passwordErrorLabel.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+           passwordErrorLabel.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+           passwordErrorLabel.heightAnchor.constraint(equalToConstant: 20),
+           
            confirmPasswordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
            confirmPasswordTextField.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
            confirmPasswordTextField.widthAnchor.constraint(equalToConstant: 200),
            confirmPasswordTextField.heightAnchor.constraint(equalToConstant: 40),
-
+           
+           confirmPasswordErrorLabel.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: 2),
+           confirmPasswordErrorLabel.leadingAnchor.constraint(equalTo: confirmPasswordTextField.leadingAnchor),
+           confirmPasswordErrorLabel.trailingAnchor.constraint(equalTo: confirmPasswordTextField.trailingAnchor),
+           confirmPasswordErrorLabel.heightAnchor.constraint(equalToConstant: 20),
+           
            signupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
            signupButton.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: 20),
            signupButton.widthAnchor.constraint(equalToConstant: 200),
@@ -134,11 +181,92 @@ class SignupViewController: UIViewController {
         }
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == emailTextField {
+            // Validate email on each change
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            validateEmail(text: newText)
+        }
+
+        if textField == passwordTextField {
+            // validate password on each change
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            validatePassword(text: newText)
+        }
+        
+        if textField == confirmPasswordTextField {
+            // validate the confirm password field
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            validateConfirmPassword(text: newText)
+        }
+
+        return true
+    }
+
+    func validateEmail(text: String) {
+        if text.isValidEmail() {
+            emailErrorLabel.isHidden = true // Hide error message if email is valid
+            emailTextField.textColor = .black // Reset text color
+        } else {
+            emailErrorLabel.isHidden = false // Show error message
+            emailErrorLabel.text = "Invalid email format" // Set error message text
+            emailTextField.textColor = .red // Change text color to indicate error
+        }
+    }
+    
+    func validatePassword(text: String) {
+        if text.isValidPassword() {
+            passwordErrorLabel.isHidden = true // Hide error message if email is valid
+            passwordTextField.textColor = .black // Reset text color
+        } else {
+            passwordErrorLabel.isHidden = false // Show error message
+            passwordErrorLabel.text = "Invalid password format" // Set error message text
+            passwordTextField.textColor = .red // Change text color to indicate error
+        }
+    }
+    
+    func validateConfirmPassword(text: String) {
+        if text.isValidConfirmPassword(password: passwordTextField.text ?? "") {
+            // Passwords match
+            confirmPasswordErrorLabel.isHidden = true
+            confirmPasswordTextField.textColor = .black
+        } else {
+            // Passwords don't match
+            confirmPasswordErrorLabel.isHidden = false
+            confirmPasswordErrorLabel.text = "Passwords do not match"
+            confirmPasswordTextField.textColor = .red
+        }
+    }
+    
     // Function to show a pop-up for password mismatch
     private func showPasswordMismatchAlert() {
         let alert = UIAlertController(title: "Error", message: "Password and Confirm Password do not match.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Close", style: .default, handler: nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
+    }
+}
+
+extension String {
+    func isValidEmail() -> Bool {
+        // Regular expression pattern for email validation
+        let emailRegex = #"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$"#
+        
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: self)
+    }
+    func isValidPassword() -> Bool {
+        // Password requirements:
+        // At least 6 characters, one uppercase letter, one lowercase letter, one number, and one special character
+        // change to actual regex
+        let passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,}"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: self)
+    }
+    func isValidConfirmPassword(password: String) -> Bool {
+        return self == password
     }
 }
